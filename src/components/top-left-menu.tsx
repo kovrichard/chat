92278c549164bbd6a00f48189b4cf54216763c13
Richard Kovacs <richardkovacs.it@gmail.com>
2025-03-29 15:31:04 +0100

    feat(top-menu): add theme toggle

--- src/components/top-left-menu.tsx
+++ src/components/top-left-menu.tsx
@@ -1,10 +1,41 @@
+"use client";
+
 import { cn } from "@/lib/utils";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { SidebarTrigger } from "./ui/sidebar";
 
 export function TopLeftMenu() {
+  const { setTheme } = useTheme();
+
   return (
-    <div className={cn("fixed left-0 grid place-items-center flex-1 pl-4 pt-3.5 z-10")}>
+    <div
+      className={cn(
+        "fixed left-0 right-0 flex justify-between items-center flex-1 px-4 pt-3.5 z-10"
+      )}
+    >
       <SidebarTrigger />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon" className="rounded-full">
+            <Sun className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
