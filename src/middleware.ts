5537ba01d36ca96dd0af9450594222cd274e1b05
Richard Kovacs <richardkovacs.it@gmail.com>
2025-03-30 00:40:53 +0100

    feat: add password auth for now

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { auth } from "@/auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const publicUrls = [
   "/",
@@ -15,7 +15,38 @@ export default auth((req) => {
     const newUrl = new URL("/login", req.nextUrl.origin);
     return NextResponse.redirect(newUrl);
   }
+
+  if (!isAuthenticated(req)) {
+    return new NextResponse("Authentication required", {
+      status: 401,
+      headers: { "WWW-Authenticate": "Basic" },
+    });
+  }
 });
+
+function isAuthenticated(req: NextRequest) {
+  const authUsername = process.env.AUTH_USERNAME;
+  const authPassword = process.env.AUTH_PASSWORD;
+
+  if (!authUsername || !authPassword) {
+    return true;
+  }
+
+  const authHeader = req.headers.get("authorization") || req.headers.get("Authorization");
+
+  if (!authHeader) {
+    return false;
+  }
+
+  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+
+  const userOk = username === authUsername;
+  const passOk = password === authPassword;
+
+  return userOk && passOk;
+}
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|.*.svg|.*.png|favicon.ico).*)"],
