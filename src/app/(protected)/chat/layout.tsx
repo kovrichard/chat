9f5e61b6962f6395c9611eb1b01b171de5230e95
Richard Kovacs <richardkovacs.it@gmail.com>
2025-04-09 20:56:02 +0200

    feta(subscription): fetch initial value on server

--- src/app/(protected)/chat/layout.tsx
+++ src/app/(protected)/chat/layout.tsx
@@ -1,13 +1,16 @@
 import InputForm from "@/components/input-form";
 import { ChatProvider } from "@/lib/contexts/chat-context";
+import { getUserFromSession } from "@/lib/dao/users";
 import { ReactNode } from "react";
 
-export default function ChatLayout({ children }: { children: ReactNode }) {
+export default async function ChatLayout({ children }: { children: ReactNode }) {
+  const user = await getUserFromSession();
+
   return (
     <ChatProvider>
       <div className="flex flex-1 flex-col min-w-[320px] max-h-svh md:max-h-[calc(100svh-1rem)] bg-background md:rounded-[20px]">
         {children}
-        <InputForm />
+        <InputForm freeMessages={user?.freeMessages} />
       </div>
     </ChatProvider>
   );
--- src/components/input-form.tsx
+++ src/components/input-form.tsx
@@ -19,125 +19,130 @@ import { useQuery } from "@tanstack/react-query";
 import { v4 as uuidv4 } from "uuid";
 import { ModelMenu } from "./model-menu";
 
-const InputForm = forwardRef<HTMLTextAreaElement>((_, ref) => {
-  const router = useRouter();
-  const params = useParams();
-  const pathname = usePathname();
-  const conversationId = params.id as string;
-  const { model } = useModelStore();
-  const createConversationOptimistic = useCreateConversationOptimistic();
-  const addMessage = useAddMessage();
-  const { input, handleInputChange, handleSubmit, status, stop } = useChatContext();
-  const { data: subscription } = useQuery({
-    queryKey: ["subscription"],
-    queryFn: async () => {
-      const response = await fetch("/api/subscription");
-      return response.json();
-    },
-  });
+const InputForm = forwardRef<HTMLTextAreaElement, { freeMessages: number }>(
+  ({ freeMessages }, ref) => {
+    const router = useRouter();
+    const params = useParams();
+    const pathname = usePathname();
+    const conversationId = params.id as string;
+    const { model } = useModelStore();
+    const createConversationOptimistic = useCreateConversationOptimistic();
+    const addMessage = useAddMessage();
+    const { input, handleInputChange, handleSubmit, status, stop } = useChatContext();
+    const { data: subscription } = useQuery({
+      queryKey: ["subscription"],
+      queryFn: async () => {
+        const response = await fetch("/api/subscription");
+        return response.json();
+      },
+      initialData: {
+        freeMessages,
+      },
+    });
 
-  function handleKeyDown(e: KeyboardEvent<HTMLTextAreaElement>) {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      handleSendMessage(e);
+    function handleKeyDown(e: KeyboardEvent<HTMLTextAreaElement>) {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        handleSendMessage(e);
+      }
     }
-  }
 
-  function handleSendMessage(
-    e: FormEvent<HTMLFormElement> | KeyboardEvent<HTMLTextAreaElement>
-  ) {
-    e.preventDefault();
-    if (!input.trim()) return;
+    function handleSendMessage(
+      e: FormEvent<HTMLFormElement> | KeyboardEvent<HTMLTextAreaElement>
+    ) {
+      e.preventDefault();
+      if (!input.trim()) return;
 
-    if (pathname === "/chat") {
-      if (status !== "ready") return;
+      if (pathname === "/chat") {
+        if (status !== "ready") return;
 
-      const conversationId = uuidv4();
-      const optimisticConversation: PartialConversation = {
-        id: conversationId,
-        title: "New Chat",
-        model: model.id,
-        messages: [
-          {
+        const conversationId = uuidv4();
+        const optimisticConversation: PartialConversation = {
+          id: conversationId,
+          title: "New Chat",
+          model: model.id,
+          messages: [
+            {
+              id: uuidv4(),
+              content: input,
+              role: "user",
+              reasoning: null,
+              signature: null,
+            },
+          ],
+          lastMessageAt: new Date(),
+        };
+
+        createConversationOptimistic.mutate(optimisticConversation);
+        router.push(`/chat/${conversationId}`);
+      } else {
+        addMessage.mutate({
+          message: {
             id: uuidv4(),
             content: input,
             role: "user",
-            reasoning: null,
-            signature: null,
           },
-        ],
-        lastMessageAt: new Date(),
-      };
-
-      createConversationOptimistic.mutate(optimisticConversation);
-      router.push(`/chat/${conversationId}`);
-    } else {
-      addMessage.mutate({
-        message: {
-          id: uuidv4(),
-          content: input,
-          role: "user",
-        },
-        conversationId,
-      });
-      handleSubmit(e as FormEvent<HTMLFormElement>);
+          conversationId,
+        });
+        handleSubmit(e as FormEvent<HTMLFormElement>);
+      }
     }
-  }
 
-  return (
-    <div className="flex-none sm:pb-4 sm:px-4">
-      <form
-        onSubmit={handleSendMessage}
-        className="flex flex-col w-full max-w-5xl mx-auto items-end border rounded-t-xl sm:rounded-b-xl p-4 bg-card"
-      >
-        <TextareaAutosize
-          id="message-input"
-          ref={ref}
-          placeholder="(Shift + Enter for new line)"
-          value={input}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          rows={1}
-          className="flex min-h-10 max-h-80 w-full bg-transparent placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 text-sm resize-none"
-        />
-        <div className="flex items-center w-full gap-2">
-          <ModelMenu />
-          <div className="flex items-center ml-auto">
-            {subscription && (
+    return (
+      <div className="flex-none sm:pb-4 sm:px-4">
+        <form
+          onSubmit={handleSendMessage}
+          className="flex flex-col w-full max-w-5xl mx-auto items-end border rounded-t-xl sm:rounded-b-xl p-4 bg-card"
+        >
+          <TextareaAutosize
+            id="message-input"
+            ref={ref}
+            placeholder="(Shift + Enter for new line)"
+            value={input}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            rows={1}
+            className="flex min-h-10 max-h-80 w-full bg-transparent placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 text-sm resize-none"
+          />
+          <div className="flex items-center w-full gap-2">
+            <ModelMenu />
+            <div className="flex items-center ml-auto">
+              {subscription && (
+                <Button
+                  variant="ghost"
+                  type="button"
+                  className="text-sm text-muted-foreground h-9"
+                >
+                  {subscription.freeMessages <= 0 ? (
+                    <span>Out of messages</span>
+                  ) : subscription.freeMessages === 1 ? (
+                    <span>{subscription.freeMessages} message left</span>
+                  ) : (
+                    <span>{subscription.freeMessages} messages left</span>
+                  )}
+                </Button>
+              )}
+            </div>
+            {status === "submitted" || status === "streaming" ? (
               <Button
-                variant="ghost"
-                type="button"
-                className="text-sm text-muted-foreground h-9"
+                type="submit"
+                size="icon"
+                className="shrink-0 size-9"
+                onClick={() => stop()}
               >
-                {subscription.freeMessages <= 0 ? (
-                  <span>Out of messages</span>
-                ) : subscription.freeMessages === 1 ? (
-                  <span>{subscription.freeMessages} message left</span>
-                ) : (
-                  <span>{subscription.freeMessages} messages left</span>
-                )}
+                <IconPlayerStop size={16} />
+              </Button>
+            ) : (
+              <Button type="submit" size="icon" className="shrink-0 size-9">
+                <Send size={16} />
               </Button>
             )}
           </div>
-          {status === "submitted" || status === "streaming" ? (
-            <Button
-              type="submit"
-              size="icon"
-              className="shrink-0 size-9"
-              onClick={() => stop()}
-            >
-              <IconPlayerStop size={16} />
-            </Button>
-          ) : (
-            <Button type="submit" size="icon" className="shrink-0 size-9">
-              <Send size={16} />
-            </Button>
-          )}
-        </div>
-      </form>
-    </div>
-  );
-});
+        </form>
+      </div>
+    );
+  }
+);
 
 InputForm.displayName = "InputForm";
 
