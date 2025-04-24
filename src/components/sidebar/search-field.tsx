fafea2a1c760e3045643f0ba8d51e0df65ed5a22
Richard Kovacs <richardkovacs.it@gmail.com>
2025-04-24 22:56:18 +0200

    feat(search): add cmd k autofocus

--- src/components/sidebar/search-field.tsx
+++ src/components/sidebar/search-field.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import { debounce } from "@/lib/utils";
+import { cn, debounce } from "@/lib/utils";
 import { useSearchStore } from "@/stores/search-store";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Input } from "../ui/input";
+
+function useIsMac() {
+  const [isMac, setIsMac] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    const platform = window.navigator.platform.toLowerCase();
+    setIsMac(platform.includes("mac"));
+  }, []);
+
+  return isMac;
+}
 
 export function SearchField() {
   const [search, setSearch] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
   const { setSearchQuery } = useSearchStore();
+  const isMac = useIsMac();
   const debouncedSetSearchQuery = useCallback(
     debounce((value: string) => setSearchQuery(value), 300),
     []
@@ -17,11 +30,34 @@ export function SearchField() {
     debouncedSetSearchQuery(search || undefined);
   }, [search, debouncedSetSearchQuery]);
 
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if ((e.metaKey || e.ctrlKey) && e.key === "k") {
+        e.preventDefault();
+        inputRef.current?.focus();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
-    <Input
-      value={search}
-      onChange={(e) => setSearch(e.target.value)}
-      placeholder="Search..."
-    />
+    <div className="relative w-full">
+      <Input
+        ref={inputRef}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search..."
+      />
+      <div
+        className={cn(
+          "flex items-center justify-center absolute right-2.5 top-1/2 -translate-y-1/2 text-muted-foreground pointer-events-none px-1 py-0.5 rounded-[4px] bg-muted border",
+          isMac === undefined && "opacity-0"
+        )}
+      >
+        <span className="text-xs">{isMac ? "⌘+K" : "Ctrl+K"}</span>
+      </div>
+    </div>
   );
 }
