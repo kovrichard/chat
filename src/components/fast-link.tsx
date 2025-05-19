50a2f542ff36e8db1d5efac8220959be4f298ce4
Richard Kovacs <richardkovacs.it@gmail.com>
2025-05-19 07:26:32 +0200

    refactor(link): introduce fast link component

---
+++ src/components/fast-link.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+type FastLinkProps = LinkProps & {
+  children: React.ReactNode;
+  className?: string;
+};
+
+/**
+ * This component is a wrapper around the Next.js Link component.
+ *
+ * It achieves seemingly faster navigation by navigating on the mouse down event
+ * instead of the click event.
+ */
+export const FastLink = React.forwardRef<HTMLAnchorElement, FastLinkProps>(
+  ({ className, children, ...props }, ref) => {
+    const router = useRouter();
+
+    function handleMouseDown(e: React.MouseEvent<HTMLAnchorElement>) {
+      e.preventDefault();
+      router.push(props.href.toString());
+    }
+
+    function handleClick(e: React.MouseEvent<HTMLAnchorElement>) {
+      e.preventDefault();
+      props.onClick?.(e);
+    }
+
+    return (
+      <Link
+        ref={ref}
+        {...props}
+        onMouseDown={handleMouseDown}
+        onClick={handleClick}
+        className={className}
+      >
+        {children}
+      </Link>
+    );
+  }
+);
+
+FastLink.displayName = "FastLink";
--- src/components/sidebar/new-chat-button.tsx
+++ src/components/sidebar/new-chat-button.tsx
@@ -3,29 +3,21 @@
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/components/ui/sidebar";
 import { Plus } from "lucide-react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { FastLink } from "../fast-link";
 
 export function NewChatButton() {
   const { isMobile, setOpenMobile } = useSidebar();
-  const router = useRouter();
 
-  function handleMouseDown(e: any) {
-    e.preventDefault();
-    router.push("/chat");
-  }
-
-  function handleClick(e: any) {
-    e.preventDefault();
+  function handleClick() {
     isMobile && setOpenMobile(false);
   }
 
   return (
     <Button asChild className="w-full gap-2">
-      <Link href="/chat" prefetch onClick={handleClick} onMouseDown={handleMouseDown}>
+      <FastLink href="/chat" prefetch onClick={handleClick}>
         <Plus className="h-4 w-4" />
         New Chat
-      </Link>
+      </FastLink>
     </Button>
   );
 }
